e33cfda74ac00aea438f3506270f6bdf9de25ce9
samuelayano <samuelayano7@gmail.com>
2022-09-14 11:24:59 +0100

    added flask-blueprint

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -4,9 +4,6 @@ from myapp.app import view
 
 
 
-
-
-
 def create_app():
         # instanciating flask
     app = Flask(__name__)
@@ -14,13 +11,14 @@ def create_app():
     # directory path
     basedir = os.path.abspath(os.path.dirname(__file__))
 
-    # local sqlite database config
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
-        basedir, "data.sqlite"
-    )
+    # # local sqlite database config
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    #     basedir, "data.sqlite"
+    # )
 
-    # # heroku postgres database url
-    # app.config["DATABASE_URL"]= 'postgres://jxclkxpxucbmke:331f8a9d1c4a176b275fbddb895a2a3bfdf0dab6f5b3330e59fcacf69d844c63@ec2-34-200-205-45.compute-1.amazonaws.com:5432/dqchr6iak6dvv'
+    # heroku postgres database url
+    app.config["DATABASE_URL"]= 'postgres://jxclkxpxucbmke:331f8a9d1c4a176b275fbddb895a2a3bfdf0dab6f5b3330e59fcacf69d844c63@ec2-34-200-205-45.compute-1.amazonaws.com:5432/dqchr6iak6dvv'
+    
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config['SECRET_KEY']='weatherapp'
 
